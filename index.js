3a01a70abde2c715c4ed769384b21493c7ec7931
Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2026-01-14 21:38:09 +0600

    port in env

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
-import User from "./models/user.model.js";
 import userRouter from "./routes/user.routes.js";
 import connectDB from "./config/db.js";
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/",userRouter)
-
 
 
 app.listen(port, () => {
